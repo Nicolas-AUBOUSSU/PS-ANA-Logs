025a67eccfb2eedd85441b5585c7a78e616d6238
Nicolas AUBOUSSU <nicolas.auboussu@gmail.com>
2024-02-11 12:58:00 +0100

    sup maj

--- config/ActualiserListBox.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Actualisation de la liste des script ListBox
-  $menu.Items.Clear()
-  $scripts = Get-ChildItem -Path $scriptPath -Filter *.ps1
-  
-  $scripts | ForEach-Object {
-  $menu.Items.Add($_.Name)
-}
